281003b4387f7c5302e86aadac7597062190f4ea
Cococatty <carinauc@outlook.com>
2019-07-06 17:47:57 +1200

    stage dev

--- server.R
+++ server.R
@@ -2,6 +2,16 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
+    storeItems <- eventReactive(input$AddItems
+                                , addItemToDB(input$CourseCode, input$CourseDays, input$StartTime
+                                              , input$EndTime, input$Freq, input$cost))
+    
+    updatedTable <- eventReactive(input$ShowTable, {
+
+    })
+    
+    output$courseTimetable <- renderDataTable({updatedTable})
 
     output$distPlot <- renderPlot({
 
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyTime)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -16,6 +17,14 @@ shinyUI(fluidPage(
                         value = 30)
             , textInput("CourseCode", "Course Code")
             , selectizeInput("CourseDays", "Course Day(s)", choices = weekdaysList, multiple = TRUE)
+            , timeInput("StartTime", "Start Time", value = strptime("09:00:00", "%T"), seconds = FALSE)
+            , timeInput("EndTime", "End Time", value = strptime("11:00:00", "%T"), seconds = FALSE)
+            , sliderInput("Freq", "Frequency", min = 1, max = 5, value = 2)
+            , selectizeInput("CourseTerms", "Availablility", choice = c("S2", "Summer"))
+            , numericInput("Cost", "Cost")
+            , actionButton("AddItems", "Add Course Item")
+            # , actionButton("AddToDB", "Add Course to DB")
+            , actionButton("ShowTable", "Show Current Timetable")
         ),
 
         # Show a plot of the generated distribution
